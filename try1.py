974c0f232ae891d798979af6756c864e7aea0266
Mark Zhitomirski <mz@exactpro.com>
2020-05-31 18:39:48 +0300

    re-format per PEP-8

--- try1.py
+++ try1.py
@@ -3,10 +3,10 @@
 # pip3 install --user mysql-connector-python
 import mysql.connector
 
-dbh='127.0.0.1' # 10.44.68.150
-db1='shsha'
-dbu='shshaG'
-dbp='999'
+dbh = '127.0.0.1'  # 10.44.68.150
+db1 = 'shsha'
+dbu = 'shshaG'
+dbp = '999'
 
 cnx = mysql.connector.connect(user=dbu, password=dbp, host=dbh, database=db1)
 
@@ -24,7 +24,7 @@ tbl_conform_results = 'data_warehouse_conformance'
 cursor.execute(desc, (tbl_conform_results, db1))
 
 for (fld1, fld2) in cursor:
-  print(f'{fld1}  {fld2}')
+    print(f'{fld1}  {fld2}')
 
 conform_result_count = '''
 SELECT count(id) cnt, conformance, status, uid
@@ -32,6 +32,7 @@ FROM `data_warehouse_conformance`
 group by conformance, status, uid
 order by conformance, uid
 '''
+
 cursor.execute(conform_result_count)
 conform_rule_str = []
 conform_passed = {}
@@ -39,26 +40,26 @@ conform_other = {}
 captures = {}
 
 for (count, rstr, passed, captid) in cursor:
-  if conform_rule_str.count(rstr)<1: conform_rule_str.append(rstr)  # make index or Conformance "names"
-  crn = conform_rule_str.index(rstr)  # current Conformance index
+    if conform_rule_str.count(rstr) < 1: conform_rule_str.append(rstr)  # make index or Conformance "names"
+    crn = conform_rule_str.index(rstr)  # current Conformance index
 
-  # make 2 dicts for PASSED and others
-  if passed == 'PASSED':
-    if crn in conform_passed:
-      conform_passed[crn].append((captid, count))
+    # make 2 dicts for PASSED and others
+    if passed == 'PASSED':
+        if crn in conform_passed:
+            conform_passed[crn].append((captid, count))
+        else:
+            conform_passed[crn] = [(captid, count)]
     else:
-      conform_passed[crn] = [(captid, count)]
-  else:
-    if crn in conform_other:
-      conform_other[crn].append((captid, count))
-    else:
-      conform_other[crn] = [(captid, count)]
+        if crn in conform_other:
+            conform_other[crn].append((captid, count))
+        else:
+            conform_other[crn] = [(captid, count)]
 
-  # for each "capture_id" save PASSED/FAILED counts
-  if captid in captures:
-    captures[captid].append((crn, passed, count))
-  else:
-    captures[captid] = [(crn, passed, count)]
+    # for each "capture_id" save PASSED/FAILED counts
+    if captid in captures:
+        captures[captid].append((crn, passed, count))
+    else:
+        captures[captid] = [(crn, passed, count)]
 
 print(f'Conform Rules Passed Number {len(conform_passed)}')
 print(f'Conform Rules other (e.g. Failed) Number {len(conform_other)}')
