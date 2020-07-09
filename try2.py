f804f2b5190bed6f6df95d9ec121f3171d0ef610
Mark Zhitomirski <mz@exactpro.com>
2020-07-09 13:34:55 +0300

    filter out some tables (SQL)

--- try2.py
+++ try2.py
@@ -5,17 +5,22 @@ db1 = 'shsha'
 dbu = 'shsha'
 dbp = '999'
 
-count_approx='''
+count_approx = '''
 SELECT table_name, table_rows
 FROM information_schema.tables
 WHERE
 table_schema = DATABASE()
 and
 data_length > 0
+and table_name <> 'flyway_schema_history'
+and NOT table_name like 'SHSHA_%'
 '''
 
+tabs1 = []
 cnx = mysql.connector.connect(user=dbu, password=dbp, host=dbh, database=db1)
 cursor = cnx.cursor()
 cursor.execute(count_approx)
 for (table, rows) in cursor:
-    print(table)
+    tabs1.append(table)
+
+print(tabs1)
